0dee5c5a3503a9f7ca1fdd4210d2202f9634daa1
David Bosschaert <david.bosschaert@gmail.com>
2020-04-10 21:39:38 +0100

    Unit test start

---
+++ src/main/java/org/coderthoughts/superplaylist/PlaylistParser.java
@@ -0,0 +1,13 @@
+package org.coderthoughts.superplaylist;
+
+import java.io.StringReader;
+
+import javax.json.Json;
+
+public class PlaylistParser {
+    public static void foo() {
+        Json.createParser(new StringReader(""));
+
+    }
+
+}
---
+++ src/test/java/org/coderthoughts/superplaylist/PlaylistParserTest.java
@@ -0,0 +1,10 @@
+package org.coderthoughts.superplaylist;
+
+import org.junit.Test;
+
+public class PlaylistParserTest {
+    @Test
+    public void testParsePlaylist() {
+        System.out.println("Test");
+    }
+}
